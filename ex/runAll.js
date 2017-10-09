9cb63fbe41338ee14c3a8a0102de0022428076bd
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-10-09 16:45:15 -0700

    Deleted usless variable in ex/runAll.js

--- ex/runAll.js
+++ ex/runAll.js
@@ -3,7 +3,6 @@
 const fs       = require("fs");
 const path     = require("path");
 const basename = path.basename(module.filename);
-const alls = [];
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
