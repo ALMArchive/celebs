a059eba5fd9b7a64cc477a0408fc80f98fbc164c
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-10-18 15:52:56 -0700

    Changed celebs from a class to a simple function that returns a json file synchronously

--- test/test.celebs.js
+++ test/test.celebs.js
@@ -1,41 +1,24 @@
 "use strict"
 
 const chai = require("chai");
-const Celebs = require("../celebs.js");
+const celebs = require("../celebs.js");
 const is   = require("is");
 const passIns = ["",1,()=>{},[],{},null,undefined,Symbol("")];
 
 describe("Celebs", function() {
-   describe("Construction", function() {
-      it("object returned from Celebs constructor should be a promise.", function() {
-         const celeb = new Celebs("views","all");
-         chai.expect(celeb.constructor.name === "Promise").to.be.true;
-      });
-      it("Throw error if first parameter is not string", function() {
-         passIns.filter((elem) => is.array(elem)).map((elem) =>
-                           chai.expect(() => new Celebs(elem, "")).to.throw(Error));
-      });
-      it("Throw error if second parameter is not string", function() {
-         passIns.filter((elem) => is.array(elem)).map((elem) =>
-                           chai.expect(() => new Celebs("views", elem)).to.throw(Error));
-      });
-   });
    describe("Data Test", function() {
-      it("Should return object inside the promise", function() {
-         const celeb = new Celebs("views","all");
-         celeb.then((elem) => chai.expect(elem).to.be.an("array"));
+      it("Should return an array of objects", function() {
+         const celeb = celebs("views","all");
+         chai.expect(celeb).to.be.an("array");
+         celeb.map((elem) => chai.expect(elem).to.be.an("object"));
       });
       it("Object should have property domain for all", function() {
-         const celeb = new Celebs("no-views","all");
-         celeb.then((elem) => chai.expect(!!elem[0]).to.be.true);
-      });
-      it("Object should have property domain for domain", function() {
-         const celeb = new Celebs("no-views","domain");
-         celeb.then((elem) => chai.expect(!!elem.domain).to.be.true);
+         const celeb = celebs("no-views","all");
+         celeb.map((elem) => chai.expect(Reflect.has(elem, "domain")).to.be.true);
       });
       it("Object should have property ARTS for domain", function() {
-         const celeb = new Celebs("no-views","domain");
-         celeb.then((elem) => chai.expect(elem.domain.indexOf("ARTS")).to.not.equal(-1));
+         const celeb = celebs("no-views","domain");
+         chai.expect(celeb.domain.indexOf("ARTS")).to.not.equal(-1);
       });
    });
 });
